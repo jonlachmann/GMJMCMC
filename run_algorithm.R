47d47a2eb06e9541e3a72fce04263763913accc9
Jon <test>
2021-03-02 14:05:44 +0100

    Fixes on guide vignette Scrathpad file updated

--- run_algorithm.R
+++ run_algorithm.R
@@ -8,43 +8,81 @@ roxygenize()
 library(devtools)
 build()
 
+test()
+
 install.packages("../GMJMCMC_1.0.tar.gz")
 library(GMJMCMC)
 
-# install.packages("RSQLite")
-library(RSQLite)
+sigmoid <- function(x)exp(-x)
+sini <- function(x)sin(x/180*pi)
+expi <- function(x)exp(-abs(x))
+logi <- function(x)log(abs(x)+.Machine$double.eps)
+troot <- function(x)abs(x)^(1/3)
+to25 <- function(x)abs(x)^(2.5)
+to35 <- function(x)abs(x)^(3.5)
 
-con <- dbConnect(drv=RSQLite::SQLite(), dbname= "../scraper/dalen.db")
-tables <- dbListTables(con)
-sales <- dbGetQuery(conn=con, statement=paste0("SELECT * FROM sales"))
+transforms <- c("to25","expi","logi","to35","troot","sigmoid")
 
-sales2 <- sales[,3:8]
-sales2$x4 <- sales[,4]
-sales2$x5 <- sales[,5]
-sales2$x6 <- sales[,6]
-sales2$x7 <- sales[,7]
-sales2$x8 <- sales[,8]
-
-covmat <- cor(sales2)
-
-sigmoid<-function(x)exp(-x)
-logg <- function (x) log(abs(x)+.Machine$double.eps)
-
-transforms <- c("logg", "logg")
-
-# Generate lists of parameters and probabilities
 probs <- gen.probs.list(transforms)
 params <- gen.params.list()
 
-loglik.test <- function (data, model, formula, complex) {
-  linmod <- lm(formula = formula, data=data)
-  ret <- -AIC(linmod)
-  if (ret > 0) {
-    print(formula)
-    print(ret)
-  }
-  return(ret)
+data("breastcancer")
+
+bc <- breastcancer[,c(ncol(breastcancer),2:(ncol(breastcancer)-1))]
+
+result <- gmjmcmc(bc, logistic.loglik, transforms, 30, 1000, 5000, probs, params)
+
+result$accept
+
+summ <- summary.gmjresult(result)
+
+importance <- data.frame(c(summ$importance))
+names(summ$importance) <- summ$features
+barplot(summ$importance, las=2)
+
+summ$features
+
+library(profvis)
+profvis({result <- gmjmcmc(bc, loglik.test, transforms, 30, 20, 50, probs, params)})
+
+x1 <- rnorm(100, 1, 2)
+x2 <- rnorm(100, 3, 2)
+x3 <- rnorm(100, 5,2)
+x4 <- rnorm(100, 1, 200)
+x5 <- rnorm(100)
+x6 <- rnorm(100)
+x7 <- rnorm(100)
+x8 <- rnorm(100)
+y <- 0.5*sini(x1)+2*logi(x2+x8)+7*troot(x3)-5*x5*x6 -15
+y2 <- rbinom(100, 1, 1/(1+exp(-y)))
+
+testdata <- data.frame(cbind(y2,x1,x2,x3,x4,x5,x6,x7,x8))
+
+
+loglik.tester <- function (data, model, formula, complex) {
+  summod <- sum(which(model)) - sum(complex$width)*5
+  return(summod)
 }
+
+result <- gmjmcmc(testdata, logistic.loglik, transforms, 50, 200, 500, probs, params)
+
+summ <- summary.gmjresult(result)
+
+importance <- data.frame(c(summ$importance))
+names(summ$importance) <- summ$features
+barplot(summ$importance, las=2)
+
+greedy_kern_test <- list(probs=c(1,0,0,0,0,0),
+                      neigh.size=1, neigh.min=1, neigh.max=2)           # Greedy algorithm proposal kernel parameters
+greedy_params_test <- list(steps=30, kern=greedy_kern_test)
+
+greedy.optim(c(F,F,F,F,F,F,F,F,F,F), NULL, loglik.tester, c(T,T,T,T,T,T,T,T,T,T), NULL, greedy_params_test)
+
+sa_kern_test <- list(probs=c(1,0,0,0,0,0), neigh.size=1, neigh.min=1, neigh.max=2)
+sa_params_test <- list(t.init=100, t.min=0.00001, dt=3, M=12, kern=sa_kern_test)
+
+simulated.annealing(c(F,F,F,F,F,F,F,F,F,F), NULL, loglik.tester, c(T,T,T,T,T,T,T,T,T,T), NULL, sa_params_test)
+
 
 for(i in 1:10) sales2[,i] <- as.numeric(sales2[,i])
 
