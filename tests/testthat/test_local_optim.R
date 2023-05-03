f02eb0afefdfcdc84917c955bf5d20591b359a20
Jon <test>
2023-05-03 20:45:01 +0200

    Code style.

--- tests/testthat/test_local_optim.R
+++ tests/testthat/test_local_optim.R
@@ -4,21 +4,26 @@
 # Created on: 2021-02-25
 
 context("Local optimizers")
-library(GMJMCMC)
 
 test_that("Testing Greedy algorithm", {
-  greedy_kern_test <- list(probs=c(0,0,0,0,1,0),
-                      neigh.size=1, neigh.min=1, neigh.max=2)
-  greedy_params_test <- list(steps=8, kern=greedy_kern_test, tries=10)
+  greedy_kern_test <- list(probs = c(0,0,0,0,1,0),
+                      neigh.size = 1, neigh.min = 1, neigh.max = 2)
+  greedy_params_test <- list(steps = 8, kern = greedy_kern_test, tries = 10)
   # Dummy test likelihood function
   loglik.tester <- function (x, y, model, formula, complex, params) {
-    return(list(crit=sum(model)))
+    return(list(crit = sum(model)))
   }
+
   # Optimize empty model but dont allow all indices, should set all to true except disallowed
-  optmod <- greedy.optim(c(F,F,F,F,F,F,F,F,F,F),
-                         NULL, loglik.tester,
-                         c(F,F,T,T,T,T,T,T,T,T), NULL,
-                         greedy_params_test)$model
+  optmod <- greedy.optim(
+    c(F, F, F, F, F, F, F, F, F, F),
+    NULL,
+    loglik.tester,
+    c(F, F, T, T, T, T, T, T, T, T),
+    NULL,
+    greedy_params_test
+  )$model
+
   expect_equal(sum(optmod), 8)
-  expect_equal(optmod, c(F,F,T,T,T,T,T,T,T,T))
+  expect_equal(optmod, c(F, F, T, T, T, T, T, T, T, T))
 })
