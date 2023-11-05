bb0acb5d1f15869fec2021cefb6a0bf2319c30df
aliaksah <math.uio.no>
2023-11-05 18:06:10 +0100

    simple FBMS funtion updated

--- R/fbms.R
+++ R/fbms.R
@@ -8,6 +8,7 @@
 #' @param family The distribution family of the response variable. Currently supports "gaussian" and "binomial". Default is "gaussian".
 #' @param data A data frame containing the variables in the model. If NULL, the variables are taken from the environment of the formula. Default is NULL.
 #' @param transforms A list of transformations for BGNLM model. Default is NULL.
+#' @param loglik.pi The log-likelihood function for estimating the marginal likelihood and posterior modes (only used if family = "custom")
 #' @param loglik.alpha The log-likelihood function for the alpha parameter in the model. Default is gaussian.loglik.alpha.
 #' @param P The number of GMJMCMC generations. Default is 10.
 #' @param runs The number of parallel chains in case of parallel processing. Default is 2.
@@ -27,6 +28,7 @@
 #' @seealso \code{\link{mjmcmc}}, \code{\link{gmjmcmc}}, \code{\link{gmjmcmc.parallel}}
 #' @export
 fbms <- function(formula = NULL, family = "gaussian", data = NULL, transforms = NULL,
+                 loglik.pi = gaussian.loglik,
                  loglik.alpha = gaussian.loglik.alpha,
                  P = 10, runs = 10, cores = 1,verbose = FALSE,...)
 {
@@ -35,6 +37,8 @@ fbms <- function(formula = NULL, family = "gaussian", data = NULL, transforms = 
     loglik.pi <- gaussian.loglik
   else if(family=="binomial")
     loglik.pi <- logistic.loglik
+  else if(family == "custom")
+    loglik.pi <- loglik.pi
   if (missing(data)) 
     data <- environment(formula)
   mf <- match.call(expand.dots=FALSE)
