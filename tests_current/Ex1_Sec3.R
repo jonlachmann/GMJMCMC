a62a6d83ba6fa5a66d0f6a0df04a7c47481f3ca6
aliaksah <math.uio.no>
2025-03-06 16:31:11 +0100

    updated the database for kepler data to the most recent version

--- tests_current/Ex1_Sec3.R
+++ tests_current/Ex1_Sec3.R
@@ -14,10 +14,14 @@
 #devtools::install_github("jonlachmann/GMJMCMC@FBMS", force=T, build_vignettes=F)
 library(FBMS)
 
+data <- read.csv("https://raw.githubusercontent.com/OpenExoplanetCatalogue/oec_tables/master/comma_separated/open_exoplanet_catalogue.txt")
+data <- na.omit(data[,c("semimajoraxis","mass","radius","period","eccentricity","hoststar_mass","hoststar_radius","hoststar_metallicity","hoststar_temperature","binaryflag")]) 
+summary(data)
 
-X <- FBMS::exoplanet
-df <- as.data.frame(cbind(MajorAxis = X[,5], X[,-5]))
 
+te.ind <- 540:939
+df.train = data[-te.ind,]
+df.test = data[te.ind,]
 
 to3 <- function(x) x^3
 transforms <- c("sigmoid","sin_deg","exp_dbl","p0","troot","to3")
@@ -32,38 +36,28 @@ use.fbms = FALSE
 #
 ####################################################
 
-params <- gen.params.gmjmcmc(df)
-
-
-
-#setting residual variance fixed to the one from the Linear model
-#lmm <- lm(formula = MajorAxis ~ .,df)
-#params$loglik$var <- var(lmm$residuals)
-#set.seed(123)
-#to set variance to unknown use below
-#params$loglik$var <- "unknown"
-#to set to 1
-#params$loglik$var <- 1
+params <- gen.params.gmjmcmc(df.train)
+params$loglik$var <- "unknown"
 
 if (use.fbms) {
- result.default <- fbms(formula = MajorAxis ~ 1 + . , data = df, method = "gmjmcmc", transforms = transforms, params = params)
+ result.default <- fbms(formula = semimajoraxis ~ 1 + . , data = df.train, method = "gmjmcmc", transforms = transforms, params = params)
 } else {
- result.default <- gmjmcmc(df, transforms = transforms, params = params)
+ result.default <- gmjmcmc(df.train, transforms = transforms, params = params)
 }
 summary(result.default,labels = F)
 
 
-preds <- predict(result.default, df[,-1], link = function(x) x)
-sqrt(mean((preds$aggr$mean - df$MajorAxis)^2))
+preds <- predict(result.default, df.test[,-1], link = function(x) x)
+sqrt(mean((preds$aggr$mean - df.test$semimajoraxis)^2))
 
 #new additional ways to predict using MPM and best model
 get.best.model(result = result.default)
-preds <- predict(get.best.model(result.default), df[,-1])
-sqrt(mean((preds - df$MajorAxis)^2))
+preds <- predict(get.best.model(result.default), df.test[,-1])
+sqrt(mean((preds - df.test$semimajoraxis)^2))
 
-get.mpm.model(result = result.default,y = df$MajorAxis,x=df[,-1])
-preds <- predict(get.mpm.model(result.default,y = df$MajorAxis,x=df[,-1]), df[,-1])
-sqrt(mean((preds - df$MajorAxis)^2))
+get.mpm.model(result = result.default,y = df.test$semimajoraxis,x=df.test[,-1])
+preds <- predict(get.mpm.model(result.default,y = df.test$semimajoraxis,x=df.test[,-1]), df.test[,-1])
+sqrt(mean((preds - df.test$semimajoraxis)^2))
 
 ####################################################
 #
@@ -75,13 +69,13 @@ sqrt(mean((preds - df$MajorAxis)^2))
 set.seed(123)
 
 if (use.fbms) {
- result.P50 <- fbms(data = df, method = "gmjmcmc", transforms = transforms,
+ result.P50 <- fbms(data = df.train, method = "gmjmcmc", transforms = transforms,
                     P=50, N.init=1000, N.final=1000, params = params)
 } else {
- result.P50 <- gmjmcmc(df,  transforms = transforms,
+ result.P50 <- gmjmcmc(df.train,  transforms = transforms,
                        P=50, N.init=1000, N.final=1000, params = params)
 }
-summary(result.P50, labels = names(df)[-1])
+summary(result.P50, labels = names(df.train)[-1])
 
 ####################################################
 #
@@ -89,13 +83,13 @@ summary(result.P50, labels = names(df)[-1])
 #
 ####################################################
 
-set.seed(124)
+set.seed(123)
 if (use.fbms) {
- result_parallel <- fbms(data = df, method = "gmjmcmc.parallel", transforms = transforms,
-                         runs = 40, cores = 10, P=25,params = params)
+ result_parallel <- fbms(data = df.train, method = "gmjmcmc.parallel", transforms = transforms,
+                         runs = 40, cores = 10, P=30,params = params)
 } else {
- result_parallel <- gmjmcmc.parallel(runs = 40, cores = 10, data = df, loglik.pi = gaussian.loglik, 
-                                     transforms = transforms, P=25,params = params)
+ result_parallel <- gmjmcmc.parallel(runs = 40, cores = 10, data = df.train, loglik.pi = gaussian.loglik, 
+                                     transforms = transforms, P=30,params = params)
 }
 summary(result_parallel, tol = 0.01)
 ####################################################
@@ -108,18 +102,18 @@ summary(result_parallel, tol = 0.01)
 # summary
 
 summary(result.default)
-summary(result.default, labels = names(df)[-1])
+summary(result.default, labels = names(df.train)[-1])
 
 summary(result.P50)
 
 summary(result_parallel)
-summary(result_parallel, tol = 0.01,labels = names(df)[-1])
+summary(result_parallel, tol = 0.01,labels = names(df.train)[-1])
 
 
 ######################
 # plot
 
-pdf("result.pdf") 
+pdf.train("result.pdf.train") 
 plot(result.default)
 dev.off()
 
@@ -127,7 +121,7 @@ plot(result.default)
 
 
 
-pdf("result.P50.pdf") 
+pdf.train("result.P50.pdf.train") 
 plot(result.P50)
 dev.off()
 
@@ -135,7 +129,7 @@ plot(result.P50)
 
 
 
-pdf("result_parallel.pdf") 
+pdf.train("result_parallel.pdf.train") 
 plot(result_parallel)
 dev.off()
 
@@ -146,42 +140,42 @@ plot(result_parallel, 12)
 ######################
 # Prediction
 
-#preds <-  predict(result, df[,-1], link = function(x) x)  
-preds <-  predict(result.default, df[,-1])
+#preds <-  predict(result, df.test[,-1], link = function(x) x)  
+preds <-  predict(result.default, df.test[,-1])
 
-pdf("prediction.pdf") 
-plot(preds$aggr$mean, df$MajorAxis)
+pdf.train("prediction.pdf.train") 
+plot(preds$aggr$mean, df.test$semimajoraxis)
 dev.off()
 
-plot(preds$aggr$mean, df$MajorAxis)
+plot(preds$aggr$mean, df.test$semimajoraxis)
 
-rmse.default <- sqrt(mean((preds$aggr$mean - df$MajorAxis)^2))
+rmse.default <- sqrt(mean((preds$aggr$mean - df.test$semimajoraxis)^2))
 
 ###############################
 
 
-#preds.P50 = predict(result.P50, df[,-1], link = function(x) x)  
-preds.P50 = predict(result.P50, df[,-1])  
+#preds.P50 = predict(result.P50, df.test[,-1], link = function(x) x)  
+preds.P50 = predict(result.P50, df.test[,-1])  
 
-pdf("prediction.P50.pdf") 
-plot(preds.P50$aggr$mean, df$MajorAxis)
+pdf.train("prediction.P50.pdf.train") 
+plot(preds.P50$aggr$mean, df.test$semimajoraxis)
 dev.off()
 
-plot(preds.P50$aggr$mean, df$MajorAxis)
+plot(preds.P50$aggr$mean, df.test$semimajoraxis)
 
-rmse.P50 <-  sqrt(mean((preds.P50$aggr$mean - df$MajorAxis)^2))
+rmse.P50 <-  sqrt(mean((preds.P50$aggr$mean - df.test$semimajoraxis)^2))
 
 
 ###############################
 
 
-preds.multi <- predict(result_parallel , df[,-1], link = function(x) x)
+preds.multi <- predict(result_parallel , df.test[,-1], link = function(x) x)
 
-pdf("pred_parallel.pdf") 
-plot(preds.multi$aggr$mean, df$MajorAxis)
+pdf.train("pred_parallel.pdf.train") 
+plot(preds.multi$aggr$mean, df.test$semimajoraxis)
 dev.off()
 
-rmse.parallel <- sqrt(mean((preds.multi$aggr$mean - df$MajorAxis)^2))
+rmse.parallel <- sqrt(mean((preds.multi$aggr$mean - df.test$semimajoraxis)^2))
 
 c(rmse.default, rmse.P50, rmse.parallel)
 
