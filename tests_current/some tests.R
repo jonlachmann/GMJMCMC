3eb5b5b256371373ff72c593ea8c12fbdc7d56d0
aliaksah <math.uio.no>
2025-04-05 14:34:19 +0200

    tests added

--- tests_current/some tests.R
+++ tests_current/some tests.R
@@ -42,7 +42,7 @@ families <- c("gaussian", "binomial", "poisson", "gamma")
 
 # Required parameters for priors
 prior_params <- list(
-  "g-prior" = list(g = 10),
+  "g-prior" = list(g = 50,a = 50),
   "hyper-g" = list(a = 3),
   "hyper-g-n" = list(a = 3),
   "ZS-null" = list(a = 3),
@@ -70,22 +70,38 @@ for (family in families) {
     
     params <- list(family = family,beta_prior = list(type = prior))
     
+    params_old <- list(family = family,prior_beta = prior)
+    
+    
     # Add required parameters if applicable
    
     if (prior %in% names(prior_params)) {
       params$beta_prior <- c(params$beta_prior, prior_params[[prior]])
     }
     
+    if (prior %in% names(prior_params)) {
+      params_old <- c(params_old, prior_params[[prior]])
+    }
+    
     # Run the model
     tryCatch({
-      result <- fbms.mlik.master2(data$y, data$x, model = c(T, rep(TRUE, ncol(data$x) - 1)), 
+      
+      set.seed(1)
+      result <- fbms.mlik.master(data$y, data$x, model = c(T, rep(TRUE, ncol(data$x) - 1)), 
                                  complex = list(oc = 1), params = params)
-      result.null <- fbms.mlik.master2(data$y, data$x, model = c(T, T, rep(FALSE, ncol(data$x) - 2)), 
-                                      complex = list(oc = 1), params = params)
+      set.seed(1)
+      result.null <- fbms.mlik.master(data$y, data$x, model = c(T, T, rep(FALSE, ncol(data$x) - 2)), 
+                        complex = list(oc = 1), params = params)
+      set.seed(1)
+      result.old <- fbms.mlik.master_old(data$y, data$x, model = c(T, rep(TRUE, ncol(data$x) - 1)), 
+                                       complex = list(oc = 1), params = params_old)#
+      set.seed(1)
+      result.null.old <- fbms.mlik.master_old(data$y, data$x, model = c(T, T, rep(FALSE, ncol(data$x) - 2)), 
+                       complex = list(oc = 1), params = params_old)
       
       
-      crit_rounded <- round(result$crit - result.null$crit, 8) 
-      coefs_mean <- round(mean(result$coefs) - mean(result.null$coefs), 8)
+      crit_rounded <- round(result$crit - result.null$crit - result.old$crit + result.null.old$crit, 8) 
+      coefs_mean <- round(mean(result$coefs) - mean(result.null$coefs) - mean(result.old$coefs) + mean(result.null.old$coefs), 8)
       
       cat(sprintf("\nPrior: %-15s -> crit: %8.4f, mean(coefs): %8.4f", prior, crit_rounded, coefs_mean))
       
