f1310c6bb8669c717dba6110d4cf1c1afefc671e
aliaksah <math.uio.no>
2025-07-03 14:28:06 +0200

    added the graphics arguments for plotting passing

--- R/diagnostics.R
+++ R/diagnostics.R
@@ -3,45 +3,91 @@
 # Created by: jonlachmann
 # Created on: 2021-02-24
 
-# TODO: Inter-thread variance comparison of the marginal log posterior of the best found model.
 
 #' Plot convergence of best/median/mean/other summary log posteriors in time 
 #'
 #' @param res Object corresponding gmjmcmc output
 #' @param FUN The summary statistics to check convergence
-#' @param conf which confidence intervals to plot
-#' @param burnin how many first populations to skip
-#' @param window sliding window for computing the standard deviation
-#' @param ylim limits for the plotting range, if unspecified, min and max of confidence intervals will be used
+#' @param conf Which confidence intervals to plot
+#' @param burnin How many first populations to skip
+#' @param window Sliding window for computing the standard deviation
+#' @param ylim Limits for the plotting range; if unspecified, min and max of confidence intervals will be used
+#' @param ... Additional graphical parameters passed to plot and lines functions, e.g. col, lwd, lty, main, xlab, ylab, ylim
 #'
 #' @return A list of summary statistics for checking convergence with given confidence intervals
 #' 
 #' @examples
 #' result <- gmjmcmc(y = matrix(rnorm(100), 100),
-#' x = matrix(rnorm(600), 100), 
-#' P = 2, 
-#' transforms =  c("p0", "exp_dbl"))
-#' diagnstats <- diagn_plot(result)
+#'                   x = matrix(rnorm(600), 100), 
+#'                   P = 2, 
+#'                   transforms =  c("p0", "exp_dbl"))
+#' diagnstats <- diagn_plot(result, col = "blue", lwd = 2, main = "Custom Title")
 #' 
 #' @export
-diagn_plot <- function (res, FUN = median, conf = 0.95, burnin = 0, window = 5, ylim = NULL) {
+diagn_plot <- function(res, FUN = median, conf = 0.95, burnin = 0, window = 5, ylim = NULL, ...) {
   
-  if(length(res$thread.best)>0)
+  args <- list(...)
+  args[["..."]] <- NULL  # Remove any "..." element to avoid warning
+  
+  if(length(res$thread.best) > 0)
     matrix.results <- res$best.log.posteriors
   else
     matrix.results <- as.matrix(unlist(res$best.margs))
-  sr <- sapply((1+burnin):dim(matrix.results)[1], FUN = function(x)FUN(matrix.results[x,]))
-  sds <- c(0,sapply(2:length(sr), function(x)sd(sr[max(1,x-window):x])))
   
-  ub <- sr + qnorm(p = 1-(1-conf)/2)*sds
-  lb <- sr - qnorm(p = 1-(1-conf)/2)*sds
+  sr <- sapply((1 + burnin):dim(matrix.results)[1], function(x) FUN(matrix.results[x, ]))
+  sds <- c(0, sapply(2:length(sr), function(x) sd(sr[max(1, x - window):x])))
   
-  if(length(ylim)==0)
+  ub <- sr + qnorm(1 - (1 - conf) / 2) * sds
+  lb <- sr - qnorm(1 - (1 - conf) / 2) * sds
+  
+  if (is.null(ylim) && is.null(args$ylim))
     ylim <- c(min(lb), max(ub))
+  else if (is.null(ylim))
+    ylim <- args$ylim
   
-  plot(y = sr,x = (burnin+1):(dim(matrix.results)[1]), type = "l",col = 1,ylim = ylim, main = "Convergence", xlab = "Population", ylab = "Summary")
-  lines(y = ub,x = (burnin+1):(dim(matrix.results)[1]),col = 1,lty = 2)
-  lines(y = lb,x = (burnin+1):(dim(matrix.results)[1]),col = 1,lty = 2)
+  main <- if (!is.null(args$main)) args$main else "Convergence"
+  xlab <- if (!is.null(args$xlab)) args$xlab else "Population"
+  ylab <- if (!is.null(args$ylab)) args$ylab else "Summary"
+  
+  args$main <- NULL
+  args$xlab <- NULL
+  args$ylab <- NULL
+  args$ylim <- NULL
+  
+  do.call(plot, c(
+    list(
+      y = sr,
+      x = (burnin + 1):(dim(matrix.results)[1]),
+      type = "l",
+      col = 1,
+      ylim = ylim,
+      main = main,
+      xlab = xlab,
+      ylab = ylab
+    ),
+    args
+  ))
+  
+  do.call(lines, c(
+    list(
+      y = ub,
+      x = (burnin + 1):(dim(matrix.results)[1]),
+      col = 1,
+      lty = 2
+    ),
+    args
+  ))
+  
+  do.call(lines, c(
+    list(
+      y = lb,
+      x = (burnin + 1):(dim(matrix.results)[1]),
+      col = 1,
+      lty = 2
+    ),
+    args
+  ))
   
   return(list(stat = sr, lower = lb, upper = ub))
 }
+
