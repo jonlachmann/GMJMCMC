fce07cf8d402b5b99e264799b4762df872e2615a
aliaksah <math.uio.no>
2025-07-18 16:51:12 +0200

    example 10 updated by Florian

--- tests_current/Ex10_Sec6_3.R
+++ tests_current/Ex10_Sec6_3.R
@@ -1,8 +1,8 @@
 #######################################################
 #
-# Example 11 (Section 6.3): Epil data set from the INLA package
+# Example 10 (Section 6.3): Epil data set from the INLA package
 #
-# Mixed Effect Poisson Model with Fractional Polynomials
+# Mixed Effect Poisson Model with Fractional Polynomials, using only fbms
 #
 # This is the valid version for the JSS Paper
 #
@@ -11,7 +11,6 @@
 library(FBMS)
 library(INLA)
 library(tictoc)
-use.fbms <- FALSE  
 
 data <- INLA::Epil
 data <- data[,-c(5,6)]
@@ -20,10 +19,6 @@ df <- data[1:5]
 df$V2 <- rep(c(0,1,0,0),59)
 df$V3 <- rep(c(0,0,1,0),59)
 df$V4 <- rep(c(0,0,0,1),59)
-
-
-#df$Trt.Base <- df$Trt * df$Base
-#df$Trt.Age <- df$Trt * df$Age
 
 transforms <- c("p0","p2","p3","p05","pm05","pm1","pm2","p0p0","p0p05","p0p1","p0p2","p0p3","p0p05","p0pm05","p0pm1","p0pm2")
 probs <- gen.probs.gmjmcmc(transforms)
@@ -37,10 +32,9 @@ params$greedy$tries <- 1
 params$sa$t.min <- 0.1
 params$sa$dt <- 10
 
-#estimator function
+# function to estimate log posterior
 poisson.loglik.inla <- function (y, x, model, complex, mlpost_params) 
 {
-
   if(sum(model)>1)
   {
     data1 <- data.frame(y, as.matrix(x[,model]), mlpost_params$PID)
@@ -57,13 +51,13 @@ poisson.loglik.inla <- function (y, x, model, complex, mlpost_params)
   
   mod<-NULL
   
-  #importance with error handling for unstable libraries that one does not trust 100%
+  #error handling for unstable libraries that might crash
   tryCatch({
-    mod <- inla(family = "poisson",silent = 1L,safe = F, data = data1,formula = formula1)
+    mod <- inla(family = "poisson",silent = 1L,safe = F,data = data1,formula = formula1)
   }, error = function(e) {
     # Handle the error by setting result to NULL
     mod <- NULL
-    # You can also print a message or log the error if needed
+    # Print a message or log the error if needed
     cat("An error occurred:", conditionMessage(e), "\n")
   })
   
@@ -82,17 +76,11 @@ poisson.loglik.inla <- function (y, x, model, complex, mlpost_params)
 set.seed(03052024)
 #specify indices for a random effect
 
-if (use.fbms) {
-  result <- fbms(formula = y ~ 1 + ., data = df, family = "custom", loglik.pi = poisson.loglik.inla,
-                 model_prior = list(r = 1/dim(df)[1],PID = data$Ind, INLA.num.threads = 10),
-                 method = "gmjmcmc", 
-                 transforms = transforms, probs = probs, params = params, P=3)
-} else {
-  result <- gmjmcmc(x = df[, -1], y = df[, 1], loglik.pi = poisson.loglik.inla, 
-                 mlpost_params = list(r = 1/dim(df)[1],PID = data$Ind, INLA.num.threads = 10),
-                 transforms = transforms,
-                 probs = probs, params = params, P = 3)
-}
+result <- fbms(formula = y ~ 1+., data = df, transforms = transforms,
+               method = "gmjmcmc", probs = probs, params = params, P=25, N = 100,
+               family = "custom", loglik.pi = poisson.loglik.inla,
+               model_prior = list(r = 1/dim(df)[1]), 
+               extra_params = list(PID = data$Ind, INLA.num.threads = 1))
 
 plot(result)
 summary(result)
@@ -102,21 +90,13 @@ set.seed(23052024)
 
 tic()
 # Number of threads used by INLA set to 1 to avoid conflicts between two layers of parallelization
-if (use.fbms) {
-  result2 <- fbms(formula = y ~ 1 + ., data = df, family = "custom", loglik.pi = poisson.loglik.inla,
-                  model_prior = list(r = 1/dim(df)[1],PID = data$Ind, INLA.num.threads = 1),
-                  method = "gmjmcmc.parallel", runs = 40, cores = 40, 
-                  transforms = transforms, probs = probs, params = params, P=25)
-} else {
-  result2 <- gmjmcmc.parallel(runs = 40, cores = 40, x = df[, -1], y = df[, 1], 
-                              loglik.pi = poisson.loglik.inla,
-                              mlpost_params = list(r = 1/dim(df)[1],PID = data$Ind, INLA.num.threads = 1),
-                              transforms = transforms, probs = probs, params = params, P = 25)
-}
+result2 <- fbms(formula = y ~ 1+., data = df, transforms = transforms,
+                probs = probs, params = params, P=25, N = 100,
+                method = "gmjmcmc.parallel", runs = 40, cores = 40,
+                family = "custom", loglik.pi = poisson.loglik.inla,
+                model_prior = list(r = 1/dim(df)[1]), 
+                extra_params = list(PID = data$Ind, INLA.num.threads = 1))
 time.inla <- toc()
 
 plot(result2)
 summary(result2, labels = names(df)[-1], tol = 0.01)
-
-
-
